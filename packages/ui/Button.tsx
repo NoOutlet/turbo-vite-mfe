2ad0283ca31ced3a0f9f0df515f536d74506eb0b
Jon Anderson <jon.anderson@willowtreeapps.com>
2023-06-11 01:13:29 -0400

    Start over

--- packages/ui/Button.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-export const Button = () => {
-  return <button>Boom</button>;
-};
-
-export const add = (a: number, b: number) => a + b;
--- packages/ui/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-export * from "./Button";
-
-// utils
-export { setupCounter } from "./utils/counter";
-
-// components
-export { Header } from "./header";
-export { Counter } from "./counter";
